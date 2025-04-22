19090945d26ee3d0abd467b3b4fbf75d672fbc4c
gratisvictory <gratisvictory.dev@proton.me>
2025-04-22 22:04:21 +0400

    ci: release

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,9 @@
 /** @type {import('lint-staged').Config} */
 const config = {
-	'*.{js,ts,json,md,yml,yaml}': [
+	'*/**': [
 		'prettier --write --cache --cache-location=node_modules/.cache/.prettiercache --log-level=warn',
+		() => 'bunx sherif@latest',
 	],
-	'*/**': () => 'bunx sherif@latest',
 };
 
 export default config;
