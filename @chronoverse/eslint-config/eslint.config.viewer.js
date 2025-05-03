a1063552205c513b374a4ba8400f9071c5557918
gratisvictory <gratisvictory.dev@proton.me>
2025-05-04 00:58:45 +0400

    chore(eslint-config): add css plugin to ESLint configuration add css plugin to ESLint configuration

--- @chronoverse/eslint-config/eslint.config.viewer.js
+++ @chronoverse/eslint-config/eslint.config.viewer.js
@@ -1,11 +1,12 @@
 import { prettier } from '@chronoverse-eslint/prettier';
 import { defineConfig } from 'eslint/config';
-import { jsdocForTs, nextTSX, tailwindcss } from './src/index.js';
+import { css, jsdocForTs, nextTSX, tailwindcss } from './src/index.js';
 
 export default defineConfig([
 	nextTSX,
 	jsdocForTs,
 	tailwindcss,
+	css,
 	/**
 	 * prettier last position plugin
 	 */
