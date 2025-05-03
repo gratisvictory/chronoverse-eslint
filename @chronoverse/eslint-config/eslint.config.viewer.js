f11f1decb438c3d135be2d0c4e91973099b77ad6
gratisvictory <gratisvictory.dev@proton.me>
2025-05-04 01:56:10 +0400

    chore(eslint-config): release@2.3.2 and remove Tailwind CSS support from ESLint configuration

--- @chronoverse/eslint-config/eslint.config.viewer.js
+++ @chronoverse/eslint-config/eslint.config.viewer.js
@@ -1,11 +1,10 @@
 import { prettier } from '@chronoverse-eslint/prettier';
 import { defineConfig } from 'eslint/config';
-import { css, jsdocForTs, nextTSX, tailwindcss } from './src/index.js';
+import { css, jsdocForTs, nextTSX } from './src/index.js';
 
 export default defineConfig([
 	nextTSX,
 	jsdocForTs,
-	tailwindcss,
 	css,
 	/**
 	 * prettier last position plugin
