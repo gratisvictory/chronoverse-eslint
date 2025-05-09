0618ccba44b288a876222b30e59eb0323c59733c
gratisvictory <gratisvictory.dev@proton.me>
2025-05-09 18:10:43 +0400

    chore: simplify Prettier and ESLint commands by removing cache options

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,10 +2,11 @@ import { JAVASCRIPT_FILES, JSON_FILES } from '@chronoverse-eslint/shared';
 
 const config = {
 	[JAVASCRIPT_FILES]: [
-		'prettier --write --cache --cache-location=node_modules/.cache/.prettiercache --log-level=warn',
-		'eslint --fix --cache --cache-location=node_modules/.cache/.eslintcache',
+		'prettier --write --log-level=warn',
+		'eslint --fix',
 	],
-	[JSON_FILES]: ['prettier --write --cache --cache-location=node_modules/.cache/.prettiercache --log-level=warn'],
+	[JSON_FILES]: ['prettier --write --log-level=warn'],
 	'*': () => 'bunx sherif@latest',
 };
+
 export default config;
